47edbe935504fc1935b3e90b0f1da789c3f3d84f
Maria Bordunova <triphonovamaria@mail.ru>
2021-12-03 20:49:35 +0300

    Deprecated setUserId method

--- Runtime/Android/Plugins/com/qonversion/unitywrapper/QonversionWrapper.java
+++ Runtime/Android/Plugins/com/qonversion/unitywrapper/QonversionWrapper.java
@@ -35,7 +35,7 @@ import com.qonversion.android.sdk.QonversionPermissionsCallback;
 import com.qonversion.android.sdk.dto.QLaunchResult;
 import com.qonversion.android.sdk.dto.QPermission;
 
-import androidx.preference.PreferenceManager;
+import android.preference.PreferenceManager;
 
 public class QonversionWrapper {
     public static String TAG = "QonversionWrapper";
@@ -79,10 +79,6 @@ public class QonversionWrapper {
         Qonversion.setDebugMode();
     }
 
-    public static synchronized void setUserID(String userID) {
-        Qonversion.setUserID(userID);
-    }
-
     public static synchronized void setProperty(String key, String value) {
         try {
             QUserProperties enumKey = QUserProperties.valueOf(key);
@@ -112,10 +108,6 @@ public class QonversionWrapper {
         } catch (Exception e) {
             Log.e(TAG, "pushAttribution error: " + e.getLocalizedMessage());
         }
-    }
-
-    public static synchronized void setUserId(String value) {
-        Qonversion.setUserID(value);
     }
 
     public static synchronized void checkPermissions(String unityCallbackName) {
