4f30cc068e541cd1d61c4c8aedf258dd207fe248
Surik <surik.sarkisyan@gmail.com>
2022-03-11 16:26:23 +0300

    Added automations event and event type entities

---
+++ Runtime/Scripts/Models/AutomationsEvent.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QonversionUnity
+{
+    public class AutomationsEvent
+    {
+        public readonly AutomationsEventType Type;
+        public readonly DateTime Date;
+
+        public AutomationsEvent(Dictionary<string, object> dict)
+        {
+
+            if (dict.TryGetValue("timestamp", out value)) Date = Utils.FormatDate((long)time);
+            if (dict.TryGetValue("type", out eventType)) Type = FormatAutomationsEventType(eventType);
+
+        }
+
+        private AutomationsEventType FormatAutomationsEventType(object type) {
+            string value = type as string;
+            AutomationsEventType result;
+            switch (value)
+            {
+                case "trial_started":
+                    result = AutomationsEventType.TrialStarted;
+                    break;
+                case "trial_converted":
+                    result = AutomationsEventType.TrialConverted;
+                    break;
+                case "trial_canceled":
+                    result = AutomationsEventType.TrialCanceled;
+                    break;
+                case "trial_billing_retry_entered":
+                    result = AutomationsEventType.TrialBillingRetry;
+                    break;
+                case "subscription_started":
+                    result = AutomationsEventType.SubscriptionStarted;
+                    break;
+                case "subscription_renewed":
+                    result = AutomationsEventType.SubscriptionRenewed;
+                    break;
+                case "subscription_refunded":
+                    result = AutomationsEventType.SubscriptionRefunded;
+                    break;
+                case "subscription_canceled":
+                    result = AutomationsEventType.SubscriptionCanceled;
+                    break;
+                case "subscription_billing_retry_entered":
+                    result = AutomationsEventType.SubscriptionBillingRetry;
+                    break;
+                case "in_app_purchase":
+                    result = AutomationsEventType.InAppPurchase;
+                    break;
+                case "subscription_upgraded":
+                    result = AutomationsEventType.SubscriptionUpgraded;
+                    break;
+                case "trial_still_active":
+                    result = AutomationsEventType.TrialStillActive;
+                    break;
+                case "trial_expired":
+                    result = AutomationsEventType.TrialExpired;
+                    break;
+                case "subscription_expired":
+                    result = AutomationsEventType.SubscriptionExpired;
+                    break;
+                case "subscription_downgraded":
+                    result = AutomationsEventType.SubscriptionDowngraded;
+                    break;
+                case "subscription_product_changed":
+                    result = AutomationsEventType.SubscriptionProductChanged;
+                    break;
+                default:
+                    result = AutomationsEventType.Unknown;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Runtime/Scripts/Models/AutomationsEventType.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace QonversionUnity
+{
+    public enum AutomationsEventType
+    {
+
+    }
+
+    private AutomationsEventType FormatAutomationsEventType(object type) {
+            string value = type as string;
+            AutomationsEventType result;
+            switch (value)
+            {
+                case "trial_started":
+                    result = AutomationsEventType.;
+                    break;
+                case "trial_converted":
+                    result = EligibilityStatus.Eligible;
+                    break;
+                case "trial_canceled":
+                    result = EligibilityStatus.Ineligible;
+                    break;
+                case "trial_billing_retry_entered":
+                    result = EligibilityStatus.NonIntroProduct;
+                    break;
+                case "subscription_started":
+                    result = EligibilityStatus.Eligible;
+                    break;
+                case "subscription_renewed":
+                    result = EligibilityStatus.Ineligible;
+                    break;
+                case "subscription_refunded":
+                    result = EligibilityStatus.NonIntroProduct;
+                    break;
+                case "subscription_canceled":
+                    result = EligibilityStatus.Eligible;
+                    break;
+                case "subscription_billing_retry_entered":
+                    result = EligibilityStatus.Ineligible;
+                    break;
+                case "in_app_purchase":
+                    result = EligibilityStatus.NonIntroProduct;
+                    break;
+                case "subscription_upgraded":
+                    result = EligibilityStatus.Eligible;
+                    break;
+                case "trial_still_active":
+                    result = EligibilityStatus.Ineligible;
+                    break;
+                case "trial_expired":
+                    result = EligibilityStatus.NonIntroProduct;
+                    break;
+                case "subscription_expired":
+                    result = EligibilityStatus.Eligible;
+                    break;
+                case "subscription_downgraded":
+                    result = EligibilityStatus.Ineligible;
+                    break;
+                case "subscription_product_changed":
+                    result = EligibilityStatus.Ineligible;
+                    break;
+                default:
+                    result = EligibilityStatus.Unknown;
+                    break;
+            }
+
+            return result;
+        }
+}
