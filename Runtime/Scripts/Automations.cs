793340c46286e310041776dc9bd47158aaa5c266
Surik <surik.sarkisyan@gmail.com>
2022-03-16 17:43:29 +0300

    Removed temporary unused code

--- Runtime/Scripts/Automations.cs
+++ Runtime/Scripts/Automations.cs
@@ -1,39 +1,9 @@
-﻿using QonversionUnity.Automations;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace QonversionUnity
 {
     public partial class Automations : MonoBehaviour
     {
-        private const string GameObjectName = "AutomationsRuntimeGameObject";
-        private static IQonversionWrapper _Instance;
-        private static AutomationsDelegate automationsDelegate;
-
-        private static IQonversionWrapper getFinalInstance()
-        {
-            if (_Instance == null)
-            {
-                switch (Application.platform)
-                {
-                    case RuntimePlatform.Android:
-                        _Instance = new QonversionWrapperAndroid();
-                        break;
-                    case RuntimePlatform.IPhonePlayer:
-                        _Instance = new QonversionWrapperIOS();
-                        break;
-                    default:
-                        _Instance = new QonversionWrapperNoop();
-                        break;
-                }
-            }
-
-            GameObject go = new GameObject(GameObjectName);
-            go.AddComponent<Automations>();
-            DontDestroyOnLoad(go);
-
-            return _Instance;
-        }
-
         /// <summary>
         /// The Automations delegate is responsible for handling in-app screens and actions when push notification is received.
         /// Make sure the method is called before Qonversion.handleNotification.
