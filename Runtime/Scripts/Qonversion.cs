d666aca2abe67b81ae2159339a62ed99aa406a4c
Surik <surik.sarkisyan@gmail.com>
2022-03-30 15:09:17 +0300

    Bump version to 3.3.1

--- Runtime/Scripts/Qonversion.cs
+++ Runtime/Scripts/Qonversion.cs
@@ -51,7 +51,7 @@ namespace QonversionUnity
         private const string OnOfferingsMethodName = "OnOfferings";
         private const string OnEligibilitiesMethodName = "OnEligibilities";
 
-        private const string SdkVersion = "3.3.0";
+        private const string SdkVersion = "3.3.1";
         private const string SdkSource = "unity";
 
         private static IQonversionWrapper _Instance;
