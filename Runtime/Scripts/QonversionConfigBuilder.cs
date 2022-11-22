e7661f2cd57dac8fd013c6163e036bccd9121c66
kamospertsyan <spertsiankamo@gmail.com>
2022-11-22 11:14:02 +0600

    Small fix

--- Runtime/Scripts/QonversionConfigBuilder.cs
+++ Runtime/Scripts/QonversionConfigBuilder.cs
@@ -2,25 +2,25 @@ namespace QonversionUnity
 {
     public class QonversionConfigBuilder
     {
-        public readonly string ProjectKey;
-        public readonly LaunchMode LaunchMode;
-        public Environment Environment;
-        public EntitlementsCacheLifetime EntitlementsCacheLifetime;
+        private readonly string _projectKey;
+        private readonly LaunchMode _launchMode;
+        private Environment _environment;
+        private EntitlementsCacheLifetime _entitlementsCacheLifetime;
 
         public QonversionConfigBuilder(string projectKey, LaunchMode launchMode)
         {
-            ProjectKey = projectKey;
-            LaunchMode = launchMode;
+            _projectKey = projectKey;
+            _launchMode = launchMode;
         }
         
         /// <summary>
-        /// Set current application <see cref="Environment"/>. Used to distinguish sandbox and production users.
+        /// Set current application <see cref="_environment"/>. Used to distinguish sandbox and production users.
         /// </summary>
         /// <param name="environment">current environment.</param>
         /// <returns>builder instance for chain calls.</returns>
         public QonversionConfigBuilder SetEnvironment(Environment environment)
         {
-            Environment = environment;
+            _environment = environment;
             return this;
         }
 
@@ -28,13 +28,13 @@ namespace QonversionUnity
         /// Entitlements cache is used when there are problems with the Qonversion API
         /// or internet connection. If so, Qonversion will return the last successfully loaded
         /// entitlements. The current method allows you to configure how long that cache may be used.
-        /// The default value is <see cref="EntitlementsCacheLifetime.Month"/>.
+        /// The default value is <see cref="_entitlementsCacheLifetime.Month"/>.
         /// </summary>
         /// <param name="lifetime">desired entitlements cache lifetime duration.</param>
         /// <returns>builder instance for chain calls.</returns>
         public QonversionConfigBuilder SetEntitlementsCacheLifetime(EntitlementsCacheLifetime lifetime)
         {
-            EntitlementsCacheLifetime = lifetime;
+            _entitlementsCacheLifetime = lifetime;
             return this;
         }
 
@@ -45,10 +45,10 @@ namespace QonversionUnity
         public QonversionConfig Build()
         {
             return new QonversionConfig(
-                ProjectKey,
-                LaunchMode,
-                Environment,
-                EntitlementsCacheLifetime
+                _projectKey,
+                _launchMode,
+                _environment,
+                _entitlementsCacheLifetime
             );
         }
     }
