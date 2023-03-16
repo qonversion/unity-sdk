2a74ee835fbcaeae28e83388d3640924d8521fd2
SpertsyanKM <SpertsyanKM@users.noreply.github.com>
2023-03-16 11:58:44 +0000

    [create-pull-request] automated change

--- Runtime/Scripts/Internal/QonversionInternal.cs
+++ Runtime/Scripts/Internal/QonversionInternal.cs
@@ -21,7 +21,7 @@ namespace QonversionUnity
         private const string OnEligibilitiesMethodName = "OnEligibilities";
         private const string OnUserInfoMethodName = "OnUserInfo";
 
-        private const string SdkVersion = "4.2.0";
+        private const string SdkVersion = "4.2.1";
         private const string SdkSource = "unity";
 
         private IQonversionWrapper _nativeWrapperInstance;
