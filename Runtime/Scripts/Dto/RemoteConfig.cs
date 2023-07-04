187c9fd6db73d171c1258d9bc7f09d8c747d36a4
Surik <surik.sarkisyan@gmail.com>
2023-07-04 16:25:35 +0400

    Added nullability to Experiment

--- Runtime/Scripts/Dto/RemoteConfig.cs
+++ Runtime/Scripts/Dto/RemoteConfig.cs
@@ -7,12 +7,12 @@ namespace QonversionUnity
     public class RemoteConfig
     {
         public readonly Dictionary<object, object> Payload;
-        public readonly Experiment Experiment;
+        public readonly Experiment? Experiment;
 
         public RemoteConfig(Dictionary<string, object> dict)
         {
             if (dict.TryGetValue("payload", out object value)) Payload = value as Dictionary<object, object>;
-            if (dict.TryGetValue("experiment", out value) && value is Dictionary<string, object> experimentInfo)
+            if (dict.TryGetValue("experiment", out value) && value != null && value is Dictionary<string, object> experimentInfo)
             {
                 Experiment= new Experiment(experimentInfo);
             }
