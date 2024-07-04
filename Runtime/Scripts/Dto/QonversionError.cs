f260b774a51aa4c1bd273b67428dce6ed0562e27
Kamo Spertsyan <spertsiankamo@gmail.com>
2024-07-04 19:26:57 +0300

    Error codes comments

--- Runtime/Scripts/Dto/QonversionError.cs
+++ Runtime/Scripts/Dto/QonversionError.cs
@@ -50,43 +50,43 @@ namespace QonversionUnity
 
     public enum QErrorCode
     {
-        Unknown,
-        ApiRateLimitExceeded,
-        AppleStoreError,
-        BackendError,
-        BillingUnavailable,
-        ClientInvalid,
-        CloudServiceNetworkConnectionFailed,
-        CloudServicePermissionDenied,
-        CloudServiceRevoked,
-        FailedToReceiveData,
-        FeatureNotSupported,
-        FraudPurchase,
-        IncorrectRequest,
-        InternalError,
-        InvalidClientUid,
-        InvalidCredentials,
-        InvalidStoreCredentials,
-        LaunchError,
-        NetworkConnectionFailed,
-        OfferingsNotFound,
-        PaymentInvalid,
-        PaymentNotAllowed,
-        PlayStoreError,
-        PrivacyAcknowledgementRequired,
-        ProductAlreadyOwned,
-        ProductNotFound,
-        ProductNotOwned,
-        ProjectConfigError,
-        PurchaseCanceled,
-        PurchaseInvalid,
-        PurchasePending,
-        PurchaseUnspecified,
-        ReceiptValidationError,
-        RemoteConfigurationNotAvailable,
-        ResponseParsingFailed,
-        StoreProductNotAvailable,
-        UnauthorizedRequestData,
-        UnknownClientPlatform,
+        Unknown, // Unknown error
+        ApiRateLimitExceeded, // API requests rate limit exceeded
+        AppleStoreError, // Apple Store error received
+        BackendError, // There was a backend error
+        BillingUnavailable, // The Billing service is unavailable on the device
+        ClientInvalid, // Client is not allowed to issue the request, etc
+        CloudServiceNetworkConnectionFailed, // The device could not connect to the network
+        CloudServicePermissionDenied, // User is not allowed to access cloud service information
+        CloudServiceRevoked, // User has revoked permission to use this cloud service
+        FailedToReceiveData, // Could not receive data
+        FeatureNotSupported, // The requested feature is not supported
+        FraudPurchase, // Fraud purchase was detected
+        IncorrectRequest, // Request failed
+        InternalError, // Internal backend error
+        InvalidClientUid, // Client Uid is invalid or not set
+        InvalidCredentials, // Access token is invalid or not set
+        InvalidStoreCredentials, // This account does not have access to the requested application
+        LaunchError, // There was an error while launching Qonversion SDK
+        NetworkConnectionFailed, // There was a network issue. Make sure that the Internet connection is available on the device
+        OfferingsNotFound, // No offerings found
+        PaymentInvalid, // Purchase identifier was invalid, etc.
+        PaymentNotAllowed, // This device is not allowed to make the payment
+        PlayStoreError, // There was an issue with the Play Store service
+        PrivacyAcknowledgementRequired, // User needs to acknowledge Apple's privacy policy
+        ProductAlreadyOwned, // Failed to purchase since item is already owned
+        ProductNotFound, // Failed to purchase since the Qonversion product was not found
+        ProductNotOwned, // Failed to consume purchase since item is not owned
+        ProjectConfigError, // The project is not configured or configured incorrectly in the Qonversion Dashboard
+        PurchaseCanceled, // User pressed back or canceled a dialog for purchase
+        PurchaseInvalid, // Failure of purchase
+        PurchasePending, // Purchase is pending
+        PurchaseUnspecified, // Unspecified state of the purchase
+        ReceiptValidationError, // Receipt validation error
+        RemoteConfigurationNotAvailable, // Remote configuration is not available for the current user or for the provided context key
+        ResponseParsingFailed, // A problem occurred while serializing or deserializing data
+        StoreProductNotAvailable, // Requested product is not available for purchase or its product id was not found
+        UnauthorizedRequestData, // App is attempting to use SKPayment's requestData property, but does not have the appropriate entitlement
+        UnknownClientPlatform, // The current platform is not supported
     }
 }
